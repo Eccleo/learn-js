d8fbf9e2764d90b826a589b567cf597f9f7dbdce
leoniddmitriev <cezveguru@gmail.com>
2018-05-13 12:12:12 +0300

    Done  lesson 4

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -1,13 +1,15 @@
-var username=prompt('who is there?','');
-if(username == "admin"){
-    var pass=prompt('enter password','');
-    if(pass=='black majesty'){
+var username = prompt('who is there?', '');
+
+if (username === 'admin') {
+    var pass = prompt('enter password', '');
+
+    if (pass === 'black majesty') {
         alert('Welcome');
-    }else if(pass==null){
+    } else if (pass == null) {
         alert('abort');
-    }else if (pass!='black majesty'){
+    } else if (pass !== 'black majesty') {
         alert('incorrect');
     }
-}else if(username!='admin'){
-    alert('Who U R bitch?');
+} else if (username !== 'admin') {
+    alert('Who R U?');
 }
