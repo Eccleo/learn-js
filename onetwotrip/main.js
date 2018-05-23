97da2b423e48fb6edaaab726f2548cd4e35456c9
leoniddmitriev <cezveguru@gmail.com>
2018-05-23 17:47:46 +0300

    Done onetwotrip

--- onetwotrip/main.js
+++ onetwotrip/main.js
@@ -12,23 +12,38 @@ function loadJson(url, callback) {
 }
 
 function showData(flightData) {
+    var i = 0;
+    var j = document.getElementById('s1').value;
+    var main = document.getElementById('main');
+    main.innerHTML = '';
 
-    var i = 1;
-    for (var i = 0; i < 11; i++) {
-        console.log(flightData.flights[i].id);
-        console.log(flightData.flights[i].direction.from);
-        console.log(flightData.flights[i].direction.to);
-        console.log(flightData.flights[i].arrival);
-        console.log(flightData.flights[i].departure);
-        console.log(flightData.flights[i].carrier);
-        console.log('' +
-            '')
+    for (i; i < flightData.flights.length; i++) {
+        if (j === 'All' || j === flightData.flights[i].carrier) {
+
+            var arrivalDT = new Date(Date.parse(flightData.flights[i].arrival));
+            var departureDT = new Date(Date.parse(flightData.flights[i].departure));
+            var div = main.appendChild(document.createElement('div'));
+            div.style.display = 'inline-block';
+            div.className = 'flightCard';
+            var imageBlock = div.appendChild(document.createElement('div'));
+            imageBlock.className = 'image';
+            imageBlock.id = 'image'+flightData.flights[i].id;
+            var txtBlock = div.appendChild(document.createElement('div'));
+            imageBlock.innerHTML =
+                ' <span id ="opacity">from:</span><br>' + flightData.flights[i].direction.from + '<br>' +
+                '<span id = "opacity">to:</span><br>' + flightData.flights[i].direction.to + '<br>';
+            txtBlock.innerHTML =
+                '<span id ="opacity">arrival:</span> <br>' + arrivalDT + '<br>' +
+                '<span id ="opacity">departure:</span> <br>' + departureDT + '<br>' +
+                '<span id ="opacity">company: </span> <br>' + flightData.flights[i].carrier + '<br>' + '<br>';
+      }
+
     }
+}
 
+
+function change() {
+    loadJson('data.json', showData);
 }
 
 loadJson('data.json', showData);
-
-
-
-
